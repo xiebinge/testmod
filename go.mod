2145f0b75672f678e1ea39b4a33ff115bb3fa078
jackbin <3293365448@qq.com>
2022-04-02 00:39:57 +0800

    golang test

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/xiebinge/testmod
+
+go 1.18
---
+++ test.go
@@ -0,0 +1,7 @@
+package testmod
+
+import "fmt"
+
+func Hi(name string) string { //go用到参数的地方，要考虑数据类型
+	return fmt.Sprintf("hi,%s", name)
+}
