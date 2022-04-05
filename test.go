aaa69bff739bbe5986cc95ae7eb1cc18f343a769
jackbin <3293365448@qq.com>
2022-04-05 10:10:11 +0800

    Bump version to v2

--- test.go
+++ test.go
@@ -2,11 +2,22 @@ package testmod
 
 import (
 	_ "database/sql"
+	"errors"
 	"fmt"
 )
 
-func Hi(name string) string { //go用到参数的地方，要考虑数据类型
-
-	return fmt.Sprintf("hi, %s", name)
-
+// Hi returns a friendly greeting in language lang
+func Hi(name, lang string) (string, error) {
+	switch lang {
+	case "en":
+		return fmt.Sprintf("Hi, %s!", name), nil
+	case "pt":
+		return fmt.Sprintf("Oi, %s!", name), nil
+	case "es":
+		return fmt.Sprintf("¡Hola, %s!", name), nil
+	case "fr":
+		return fmt.Sprintf("Bonjour, %s!", name), nil
+	default:
+		return "", errors.New("unknown language")
+	}
 }
